24736470f70b5c599da31daae27d87267b17b5a5
Dane Balia <enteritos@gmail.com>
2014-07-22 23:19:24 +0200

    Working version

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ### A Mass Deploy Tool for SSH ID (ssh-copy-id)
 
 A simple gem to allow the admin to deploy a mass subset of keys to multiple machines mirroring the functionality of ssh-copy-id.
+
+PS. To ignore Key Generation use -g
+
+```
+Dane-Balias-MacBook:PushSSHId danebalia$ ruby pushId.rb -g
+Connecting to 172.16.1.2 with User -> daneb.....
+daneb@172.16.1.2's password:
+Connecting to 172.16.1.3 with User -> dane.....
+dane@172.16.1.3's password:
+Done
+```
--- pushId.rb
+++ pushId.rb
@@ -1,41 +1,76 @@
-class Push
-    def generateKey 
-       system "ssh-keygen"     
-    end
-
-    def copyKey (ip, username)
-        puts "Connecting to #{ip} with User -> #{username}....."
-        system "cat ~/.ssh/id_rsa.pub | ssh #{username}@#{ip} 'mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys'"
-    end
-
-    def getHosts
-       puts "Getting hosts to process ....."
-       file = File.open("config.lst")
-       puts File.exist?("config.lst")
-       lstArray = file.readlines.map do |line|
-           RemoteClient.new( *line.split("\t") )
-        end
-    end
-
-end
-
-# Global Array 
-lstArray = Array.new
+require 'optparse'
 
 # Create our Object
 RemoteClient = Struct.new(:ip, :username)
 
+class Push
+
+    # This will generate our initial key
+    def generateKey 
+       system "ssh-keygen"     
+    end
+
+    # This will copy the key to the desired host
+    def copyKey (ip, username)
+        
+        puts "Connecting to #{ip} with User -> #{username}....."
+
+        # Magic Command - the mirror to ssh-copy-id
+        system "cat ~/.ssh/id_rsa.pub | ssh #{username}@#{ip} 'mkdir ~/.ssh; cat >> ~/.ssh/authorized_keys'"
+    end
+
+    # Will get the hosts we want to deploy the public key too
+    def getHosts
+
+       file = "config.lst"
+       if File.exist?(file)
+            lstArray = File.readlines(file).map do |line|
+            # Used ";" to avoid \t (TAB) which is different across OS'es
+            RemoteClient.new( *line.split("\;") )
+            end
+        end
+       
+    end
+
+end
+
+# Options 
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: pushId.rb [options]"
+  options[:ignore] = true
+
+  opts.on('-g', '--ignore-gen', 'Ignore SSH Key Generation') do
+    options[:ignore] = false 
+  end
+
+  # This displays the help screen, all programs are
+  # assumed to have this option.
+  opts.on( '-h', '--help', 'Display this screen' ) do
+     puts opts
+     exit
+  end
+  
+end.parse!
+
+# Array for Result 
+lstArray = Array.new
+
 # Instantiate 
 myInst = Push.new
 
-# Generate Public/Private Key
-#myInst.generateKey 
+# Generate Public/Private Key (but ignore if desired)
+if options[:ignore]
+    myInst.generateKey 
+end
 
 # Get Hosts and Push Key
-myInst.getHosts
+lstArray = myInst.getHosts
 
-# Print
-puts lstArray.empty?
+# SSH-Copy Id
 lstArray.each do |client|
-    puts client.ip
+    myInst.copyKey client.ip, client.username.strip!
 end
+
+# Confirmation
+puts "Done"
